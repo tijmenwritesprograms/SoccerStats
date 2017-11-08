827305f0f5559089e7999af578239614c405e062
Tijmen van der Hilst <ikwildeeigenlijktijmen@hotmail.com>
2017-11-08 22:33:53 +0100

    Added textfile and read/write code

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SoccerStats
 {
@@ -10,6 +11,21 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "data.txt");
+            var file = new FileInfo(fileName);
+
+            if (file.Exists)
+            {
+                using (var reader = new StreamReader(fileName))
+                {
+                    Console.SetIn(reader);
+                    Console.WriteLine(Console.ReadLine());
+                }
+
+             }
+            
         }
     }
 }
