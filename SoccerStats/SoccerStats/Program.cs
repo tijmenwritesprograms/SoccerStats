168b6e1812cb101921dd9fdb01df0870d59bf4d7
Tijmen van der Hilst <ikwildeeigenlijktijmen@hotmail.com>
2017-11-11 10:49:35 +0100

    Added GameResult class and handled DateTime

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace SoccerStats
 {
@@ -15,9 +16,7 @@ namespace SoccerStats
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
 
-            var fileContents = ReadFile(fileName);
-            Console.WriteLine(fileContents);
-            
+            var fileContents = ReadSoccerResults(fileName);
         }
 
         public static String ReadFile(String fileName)
@@ -26,6 +25,32 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<String[]> ReadSoccerResults(String fileName)
+        {
+            var SoccerResults = new List<String[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+
+                    try
+                    {
+                        gameResult.GameDate = DateTime.Parse(values[0], new CultureInfo("en-US"));
+                    }
+                    catch { }
+
+                    Console.WriteLine(gameResult.GameDate);
+                    SoccerResults.Add(values);
+                }
+            }
+
+            return SoccerResults;
         }
     }
 
